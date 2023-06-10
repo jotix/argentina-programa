15408c0436e05adc10fb9ab31e22efd627f256d0
jotix <jujodeve@gmail.com>
2023-06-10 20:44:13 -0300

    unidad3

--- unidad-3/3.1_tuplas/05.py
+++ unidad-3/3.1_tuplas/05.py
@@ -5,7 +5,8 @@ Escriba un programa que almacene el valor de tres variables
 ingresadas por el usuario en una tupla
 """
 
-var1 = 1 #input("ingrese el primer elemento de la tupla")
-var2 = 2 #input("ingrese el segundo elemento de la tupla")
-var3 = 3 #input("ingrese el tercer elemento de la tupla")
+var1 = input("ingrese el primer elemento de la tupla: ")
+var2 = input("ingrese el segundo elemento de la tupla: ")
+var3 = input("ingrese el tercer elemento de la tupla: ")
 tupla = (var1, var2, var3)
+print(tupla)
