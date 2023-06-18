ac5f28e52cf28aca414a0a39194bf9167b4fce58
jotix <jujodeve@gmail.com>
2023-06-18 09:59:39 -0300

    vim_style

---
+++ unidad-3/3.1-tuplas/03.py
@@ -0,0 +1,13 @@
+"""
+* Ejercicio 3:
+Escriba un ejemplo que muestre que las tuplas son inmutables.
+"""
+
+lista = [1, 2]
+print(id(lista))
+lista.append(2)
+print(id(lista))
+tupla = (1, 2)
+print(id(tupla))
+tupla = (2, 4)
+print(id(tupla))
