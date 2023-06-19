ee6aa0bd5082cf800683d6f4d420a50ef75b250d
jotix <jujodeve@gmail.com>
2023-06-18 22:26:26 -0300

    vim

--- unidad-3/3.1-tuplas/07.py
+++ unidad-3/3.1-tuplas/07.py
@@ -7,3 +7,11 @@ tupla?
 3. Cómo se calcula la longitud de una tupla?
 """
 
+tupla = (1, 1, 2, 3, 5, 8, 13)
+print("La tupla es:", tupla)
+print("El 5to elemento de la lista es:", tupla[5])
+print("La longitud de la lista es:", len(tupla))
+try:
+  print(tupla[10])
+except Exception as e:
+  print("Intentar acceder a una posición inexistente de la tupla da el error:", e)
--- unidad-3/3.1-tuplas/08.py
+++ unidad-3/3.1-tuplas/08.py
@@ -5,3 +5,9 @@ dupla y luego desempaquete la tupla en dos variables a y b. Luego el
 programa debe imprimir las variables a y b.
 """
 
+aux = input("ingrese un dupla, separe los elementos con una coma: ")
+tupla = tuple(aux.split(","))
+a = tupla[0]
+b = tupla[1]
+print("el elemento a de la tupla es:", a)
+print("el elemento b de la tupla es:", b)
