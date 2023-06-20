ad1f7f1b7053dbc23f5dad0fb735cad866280859
jotix <jujodeve@gmail.com>
2023-06-20 09:13:53 -0300

    3.1

--- unidad-3/3.1-tuplas/13.py
+++ unidad-3/3.1-tuplas/13.py
@@ -5,3 +5,9 @@ número a y una tupla t. Luego el programa debe mostrar por pantalla
 la cantidad de veces que aparece el número a en la tupla t.
 """
 
+a = int(input("Ingrese un numero entero: "))
+aux = input("ingrese una tupla, separe los elementos con comas: ")
+t = tuple(map(lambda elem: int(elem), aux.split(",")))
+repeticiones = t.count(a)
+glue = "no aparece" if repeticiones == 0 else ("aparece 1 vez" if repeticiones == 1 else "aparece "+str(repeticiones)+" veces")
+print("El elemento", a, glue, "en", t)
