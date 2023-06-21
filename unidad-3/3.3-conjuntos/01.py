c9dd3235c0bbfc590d9a43dd2b2629e1306881e2
jotix <jujodeve@gmail.com>
2023-06-20 22:14:12 -0300

    3.2

--- unidad-3/3.3-conjuntos/01.py
+++ unidad-3/3.3-conjuntos/01.py
@@ -6,3 +6,12 @@
 # 3. Calcule el complemento del conjunto single que contiene la pro-
 # vincia ingresada por el usuario.
 
+provincias = {"Buenos Aires", "CABA", "Catamarca", "Chaco", "Chubit", 
+              "Cordoba", "Corrientes", "Entre Rios", "Formosa", "Jujuy",
+              "La Pampa", "La Rioja", "Mendoza", "Misiones", "Neuquen",
+              "Rio Negro", "Salta", "San Juan", "San Luis", "Santa Cruz",
+              "Santiago del Estero", "Tierra del Fuego", "Tucuman"}
+
+prov = input("Ingrese una provincia: ")
+complemento = provincias - {prov,}
+print("Complemento:", complemento)
--- unidad-3/3.3-conjuntos/02.py
+++ unidad-3/3.3-conjuntos/02.py
@@ -7,3 +7,21 @@
 # 4. Calcule la intersección de A y B.
 # 5. Calcule el complemento de las unión e intersección de A y B.
  
+# punto 1
+U = set(range(1,21))
+print(U)
+# punto 2
+aux1 = input("Ingrese el conjunto A, separe los elementos con comas: ") 
+aux2 = input("Ingrese el conjunto B, separe los elementos con comas: ") 
+A = set(aux1.split(","))
+B = set(aux2.split(","))
+# punto 3
+union = A | B
+# punto 4
+interseccion = A & B
+# punto 5
+comp_union_inter = union - interseccion
+# imprimir resultados
+print("Union A | B:", union)
+print("Interseccion A & B:", interseccion)
+print("Complemento de la union e interseccion:", comp_union_inter)
