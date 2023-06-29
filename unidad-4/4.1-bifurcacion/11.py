aa81b347eeb7a6d0725f5eb072332367cdba9192
jotix <jujodeve@gmail.com>
2023-06-29 20:04:03 -0300

    refactorind_for_nvim

--- unidad-4/4.1-bifurcacion/11.py
+++ unidad-4/4.1-bifurcacion/11.py
@@ -1,21 +1,20 @@
-'''
-Ejercicio 11:
-Los tramos impositivos para la declaración de la renta en un
-determinado país son mostrados en la tabla 1.
-
-+-----------------------+-----------------+
-| Renta                 | Tipo Impositivo |
-|-----------------------+-----------------|
-| Menos de $10000       | 5 %             |
-| Entre $10000 y $20000 | 15 %            |
-| Entre $20000 y $35000 | 20 %            |
-| Entre $35000 y $60000 | 30 %            |
-| Más de $60000         | 45 %            |
-+-----------------------+-----------------+
-Cuadro 1: Tramos Impositivos
-
-Escriba un programa que pregunte al usuario su renta anual y muestre
-por pantalla el tipo impositivo que le corresponde.'''
+# Ejercicio 11:
+# Los tramos impositivos para la declaración de la renta en un
+# determinado país son mostrados en la tabla 1.
+# 
+# +-----------------------+-----------------+
+# | Renta                 | Tipo Impositivo |
+# |-----------------------+-----------------|
+# | Menos de $10000       | 5 %             |
+# | Entre $10000 y $20000 | 15 %            |
+# | Entre $20000 y $35000 | 20 %            |
+# | Entre $35000 y $60000 | 30 %            |
+# | Más de $60000         | 45 %            |
+# +-----------------------+-----------------+
+# Cuadro 1: Tramos Impositivos
+# 
+# Escriba un programa que pregunte al usuario su renta anual y muestre
+# por pantalla el tipo impositivo que le corresponde.
 
 renta = float(input("ingrese su renta anual: "))
 
--- unidad-4/4.1-bifurcacion/13.py
+++ unidad-4/4.1-bifurcacion/13.py
@@ -1,12 +1,13 @@
-'''Ejercicio 13:
-Escriba un programa para una empresa que tiene salas de
-juegos para todas las edades y quiere calcular de forma automática
-el precio que debe cobrar a sus clientes por entrar. El programa debe
-preguntar al usuario la edad del cliente y mostrar el precio de la entrada.
-Si el cliente es menor de 4 años puede entrar gratis, si tiene entre 4 y
-18 años debe pagar $5 y si es mayor de 18 años, $10.'''
+# Ejercicio 13:
+# Escriba un programa para una empresa que tiene salas de
+# juegos para todas las edades y quiere calcular de forma automática
+# el precio que debe cobrar a sus clientes por entrar. El programa debe
+# preguntar al usuario la edad del cliente y mostrar el precio de la entrada.
+# Si el cliente es menor de 4 años puede entrar gratis, si tiene entre 4 y
+# 18 años debe pagar $5 y si es mayor de 18 años, $10.
 
 edad = int(input("Ingrese la edad: "))
+
 if edad < 4:
     precio = 0
 elif edad <=18:
@@ -15,3 +16,4 @@ else:
     precio = 10
 
 print("El precio de la entrada es:", f"$ {precio}" if precio > 0 else "gratis")
+
